b1faf334ba8343a4ecfc7ec9eb412512e621e53d
Yong Gu <yong.g.gu@ericsson.com>
2022-06-19 16:32:22 +0200

    Add gprof example
    Signed-off-by: Yong Gu <yong.g.gu@ericsson.com>

---
+++ gprof/README.md
@@ -0,0 +1,5 @@
+gprof example
+==============
+
+##Links
+- [https://ftp.gnu.org/old-gnu/Manuals/gprof-2.9.1/html_mono/gprof.html]
---
+++ gprof/test.c
@@ -0,0 +1,28 @@
+#include "ftracer.h"
+
+#define mb() asm volatile ("" ::: "memory")
+
+void f3(int a, int b, int c)
+{
+	mb();
+}
+
+void f2(int a, int b, int c)
+{
+	f3(1, 2, 3);
+	f3(4, 5, 6);
+}
+
+void f1(int a, int b, int c)
+{
+	f2(10, 11, 12);
+	f2(20, 21, 22);
+	f3(7, 8, 9);
+}
+
+int main(void)
+{
+	f1(1, 2, 3);
+	f1(3, 4, 5);
+	return 0;
+}
